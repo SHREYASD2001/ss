10a2850c1801df4bab0accf081395fab14491138
Duniya@2025 <shreyashdhoke30@gmail.com>
2025-03-15 08:18:14 +0530

    Added new parameter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.logging.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         new ChromeDriver();
         try {
@@ -14,10 +17,14 @@ public class Main {
 
     @Test
     public void main1() {
+        logger.info("Hello, GitHub Actions! This is a test log.");
         new ChromeDriver();
+        logger.info("Hello, GitHub Actions! Chromedriver is started");
         try {
+            logger.info("Hello, GitHub Actions! We are waiting for 10 seconds...");
             Thread.sleep(10000);
+            logger.info("Hello, GitHub Actions! 10 seconds are completed");
         } catch (Exception ignored) {}
-        System.out.println("Hello, World!");
+        logger.info("Hello, GitHub Actions! Process is completed");
     }
 }
