90d5d8f8f1e97321a59121f09fea24c30ca770cc
Duniya@2025 <shreyashdhoke30@gmail.com>
2025-03-15 08:56:50 +0530

    reverted all the yml changes

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -4,18 +4,19 @@ import io.cucumber.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class LoginSteps {
 
     @Given("user opens portal")
     public void user_opens_portal() {
 //        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        System.out.println("Opening chrome browser");
+        RemoteWebDriver driver = new ChromeDriver();
         System.out.println("Opening Portal");
         driver.get("https://www.google.com/");
-        System.out.println("Opening Portal");
-        driver.manage().window().maximize();
         System.out.println("Maximizing Window");
+        driver.manage().window().maximize();
         // Write code here that turns the phrase above into concrete actions
 //        throw new io.cucumber.java.PendingException();
     }
