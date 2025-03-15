f6278517655c12c44b353f5894ec0d0df87464b0
Duniya@2025 <shreyashdhoke30@gmail.com>
2025-03-15 09:04:16 +0530

    Added options

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -4,15 +4,27 @@ import io.cucumber.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LoginSteps {
 
     @Given("user opens portal")
     public void user_opens_portal() {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // Generate a unique temporary user-data directory
+        Path userDataDir = Paths.get(System.getProperty("java.io.tmpdir"), "chrome-user-data-" + System.currentTimeMillis());
+        options.addArguments("--user-data-dir=" + userDataDir.toAbsolutePath().toString());
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         System.out.println("Opening chrome browser");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         System.out.println("Opening Portal");
         driver.get("https://www.google.com/");
         System.out.println("Maximizing Window");
