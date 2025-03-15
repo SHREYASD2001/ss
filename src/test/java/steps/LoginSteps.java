a753ec39b91efad4388fa9231b874740ee832a79
Duniya@2025 <shreyashdhoke30@gmail.com>
2025-03-15 08:27:31 +0530

    remove chrome download functioanlity

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,13 +1,19 @@
 package steps;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
 
     @Given("user opens portal")
     public void user_opens_portal() {
-        new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+        System.out.println("Opening Portal");
+        driver.get("google.com");
+        System.out.println("Opening Portal");
+        driver.manage().window().maximize();
+        System.out.println("Maximizing Window");
         // Write code here that turns the phrase above into concrete actions
 //        throw new io.cucumber.java.PendingException();
     }
